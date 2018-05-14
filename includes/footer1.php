4c7c0ee0b5f8a461c092e9aac9f53b87c5639104
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-05-14 16:36:19 +0700

    changement titre footer 1

--- includes/footer1.php
+++ includes/footer1.php
@@ -13,7 +13,7 @@
 				</div>
 			</div>
 			<div class="col-md-3 widget">
-				<h3 class="widget-title">Follow me</h3>
+				<h3 class="widget-title">Suivez moi</h3>
 				<div class="widget-body">
 					<p class="follow-me-icons">
 						<a href="https://github.com/nicolasdugit"><i class="fa fa-github fa-2"></i></a>
@@ -22,7 +22,7 @@
 				</div>
 			</div>
 			<div class="col-md-6 widget">
-				<h3 class="widget-title">Text widget</h3>
+				<h3 class="widget-title">À propos de ce blog</h3>
 				<div class="widget-body">
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, dolores, quibusdam architecto voluptatem amet fugiat nesciunt placeat provident cumque accusamus itaque voluptate modi quidem dolore optio velit hic iusto vero praesentium repellat commodi ad id expedita cupiditate repellendus possimus unde?</p>
 					<p>Eius consequatur nihil quibusdam! Laborum, rerum, quis, inventore ipsa autem repellat provident assumenda labore soluta minima alias temporibus facere distinctio quas adipisci nam sunt explicabo officia tenetur at ea quos doloribus dolorum voluptate reprehenderit architecto sint libero illo et hic.</p>
