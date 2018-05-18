8cf52f8333f3ff698f25cb0724330a7b33d694af
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-05-18 17:54:30 +0700

    correction redireciton index.html en index.php

--- view/frontend/editCommentView.php
+++ view/frontend/editCommentView.php
@@ -5,7 +5,7 @@
     <div class="container">
 
         <ol class="breadcrumb">
-            <li><a href="index.html">Accueil</a></li>
+            <li><a href="index.php">Accueil</a></li>
             <li class="active">Blog</li>
         </ol>
 
