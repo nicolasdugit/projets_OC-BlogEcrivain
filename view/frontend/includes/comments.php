a010883315e05e01b6fe0e0af66bfb8eaf6c32f1
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-05-18 15:50:29 +0700

    suppression fichier .php inutiles

--- view/frontend/includes/comments.php
+++
@@ -1,4 +0,0 @@
-<div class="page-header">
-		<h2>Commentaires</h2>
-</div>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, itaque, cumque, maxime obcaecati reprehenderit ea dignissimos amet voluptatem id excepturi facilis totam veritatis maiores eveniet neque explicabo temporibus quisquam in ex ab fugiat ipsa tempore sunt corporis nostrum quam illum!</p>
