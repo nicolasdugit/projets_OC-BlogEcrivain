abd21f6466d4c26ed99696a7fb59eea96350c1b8
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-01 15:43:01 +0700

    modif index.php pour redirection vers adminConnectionView.php

--- admin/index.php
+++ admin/index.php
@@ -3,9 +3,14 @@ session_start();
 require('controller/backend.php');
 
 try {
-
+	if ((isset($_GET['action']))) {
+		if ($_GET['action'] == 'connection') {
+			adminIndex();
+		}
+	}
+	else {
         adminConnection();
-  
+	}
 }
 catch(Exception $e) {
     $errorMessage = $e->getMessage();
