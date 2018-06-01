802a636e7d179dd1641f6f467bb0cfb7053d0e18
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-01 15:52:54 +0700

    modification liens relatifs vers public/

--- admin/view/backend/template.php
+++ admin/view/backend/template.php
@@ -8,20 +8,20 @@
 	
 	<title><?= $title ?></title>
 
-	<!-- <link rel="shortcut icon" href="assets/images/gt_favicon.png"> -->
+	<!-- <link rel="shortcut icon" href="public/images/gt_favicon.png"> -->
 	
 	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../public/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../public/css/font-awesome.min.css">
 
 	<!-- Custom styles for our template -->
-	<link rel="stylesheet" href="../assets/css/bootstrap-theme.css" media="screen" >
-	<link rel="stylesheet" href="../assets/css/main.css">
+	<link rel="stylesheet" href="../public/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="../public/css/main.css">
 
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
-	<script src="assets/js/html5shiv.js"></script>
-	<script src="assets/js/respond.min.js"></script>
+	<script src="public/js/html5shiv.js"></script>
+	<script src="public/js/respond.min.js"></script>
 	<![endif]-->
 </head>
 
@@ -57,8 +57,8 @@
 	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-	<script src="assets/js/headroom.min.js"></script>
-	<script src="assets/js/jQuery.headroom.min.js"></script>
-	<script src="assets/js/template.js"></script>
+	<script src="../public/js/headroom.min.js"></script>
+	<script src="../public/js/jQuery.headroom.min.js"></script>
+	<script src="../public/js/template.js"></script>
 </body>
 </html>
