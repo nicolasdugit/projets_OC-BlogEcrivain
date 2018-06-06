320f8f3502016d03a1d130acdbf79b3db6600cce
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-06 11:54:56 +0700

    suppression htmlspecialchart a l'affichage du post d'accueil

--- view/frontend/accueilView.php
+++ view/frontend/accueilView.php
@@ -13,7 +13,7 @@ $excerpt = substr($data['content'], 0, 1500);
                 <h3> <?= htmlspecialchars($data['title']) ?> </h3>
                 <h5> <em>Publié le le <?= $data['creation_date_fr'] ?></em> </h5>
                 <p>
-                    <?= nl2br(htmlspecialchars($excerpt)) , ' [...] ' ?>
+                    <?= nl2br(($excerpt)) , ' [...] ' ?>
                     <h6><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Lire la suite</a></h6>
                     <br>
                 </p>
