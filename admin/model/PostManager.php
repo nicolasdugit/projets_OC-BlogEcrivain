4d8e8fa618a9b7a81ae77f1d50176e410d0be874
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-06 09:13:55 +0700

    creation function getPosts()

--- admin/model/PostManager.php
+++ admin/model/PostManager.php
@@ -7,10 +7,9 @@ require_once("Manager.php");
 class PostManager extends Manager
 {
 
-
    public function newPost($title, $content)
    {
-        $db = $this->dbConnect();
+		$db = $this->dbConnect();
         $req = $db->prepare('INSERT INTO posts (title, content, creation_date) VALUES (:title, :content, NOW() )');
         $affectedLines  = $req->execute(array(
             ':title' => $title,
@@ -19,4 +18,12 @@ class PostManager extends Manager
 
         return $affectedLines;
    }
+
+    public function getPosts()
+    {
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC');
+
+        return $req;
+    }
 }
