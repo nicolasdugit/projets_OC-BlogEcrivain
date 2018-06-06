26e0f2d744be02e84e1aa96be2b010858c050ae2
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-06 08:55:23 +0700

    mise en forme formuliare de publication des articles et de la page adminView

--- admin/view/backend/adminView.php
+++ admin/view/backend/adminView.php
@@ -1,12 +1,21 @@
 <?php $title = 'Jean Forteroche - PAGE ADMINISTRATEUR'; ?>
 
 <?php ob_start(); ?>
+
 <h2 class="text-center top-space">Administration</h2>
 
     <div class="container">
         <div class="row">
-            <div class="col-sm-12">
-            <a href="index.php?action=createPost">Nouvel article</a>
+            <div class="col-sm-4">
+            	<h3>Actions</h3>
+            	<h4><a href="index.php?action=createPost">Nouvel article</a></h4>
+            	<h4><a href="index.php?action=createPost">Nouvel article</a></h4>
+            </div>
+            <div class="col-sm-4">
+            	<h3>Commentaires signalés</h3>
+            </div>
+            <div class="col-sm-4">
+            	<h3>Commentaires à modérer</h3>
             </div>
         </div>
     </div>
--- admin/view/backend/createPostView.php
+++ admin/view/backend/createPostView.php
@@ -6,18 +6,20 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-            <p>test</p>
 	
 			<form action="index.php?action=publishPost" method="post">
-				<label for="title">Titre du chapitre</label><br>
-				<input type="text" name="title"><br>
-				<textarea id="mytextarea" name="content">Hello, World!</textarea>
+				<div class="form-group">
+					<label for="title">Titre du chapitre :</label><br>
+					<input class="form-control" type="text" name="title"><br>
+					<textarea class="form-control" id="mytextarea" name="content">Hello, World!</textarea>
+	            </div>
 				<div>
-                    <button class="btn btn-danger" type="submit">Poster</button>
-                </div>
+					<button class="btn btn-success" type="submit">Poster</button>
+				</div>
 			</form>
 
 <h2 class="text-center top-space"><a href="../index.php">Retour a l'accueil</a></h2>
+<h2 class="text-center top-space"><a href="index.php">Retour page admin</a></h2>
 
 <?php $content = ob_get_clean(); ?>
 
