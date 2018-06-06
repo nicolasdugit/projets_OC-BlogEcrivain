65e586963291a0f130d50dd9f56a84c703fb9cdf
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-06 10:25:03 +0700

    creation function getPost()

--- admin/model/PostManager.php
+++ admin/model/PostManager.php
@@ -27,6 +27,16 @@ class PostManager extends Manager
         return $req;
     }
 
+    public function getPost($postId)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req->execute(array($postId));
+        $post = $req->fetch();
+
+        return $post;
+    }
+
     public function updatePost($title, $content)
     {
         $db = $this->dbConnect();  
