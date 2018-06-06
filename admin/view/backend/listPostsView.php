7c06cab0e50de5f11c30e2d127562f9eece11e6b
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-06 09:22:54 +0700

    suppression sidebar

--- admin/view/backend/listPostsView.php
+++ admin/view/backend/listPostsView.php
@@ -17,7 +17,7 @@
             {
             $excerpt = substr($lastPost['content'], 0, 1500);
             ?>
-            <article class="col-md-8 maincontent">
+            <article class="col-md-12 maincontent">
                 <header class="page-header">
                     <h1 class="page-title"><?= htmlspecialchars($lastPost['title']) ?></h1>
                                  <h5><em>Publié le le <?= $lastPost['creation_date_fr'] ?></em> </h5>
@@ -31,36 +31,6 @@
             <?php
             }
             ?>
-            <!-- /Article -->
-           
-            
-            <!-- Sidebar -->
-            <aside class="col-md-4 sidebar sidebar-right">
-                <?php
-                while ($data = $posts->fetch())
-                {
-                $excerpt = substr($data['content'], 0, 500);
-                ?>
-                        <div class="row widget">
-                            <div class="col-xs-12">
-                                <h4>
-                                    <?= htmlspecialchars($data['title']) ?>
-                                </h4>
-                                 <h5><em>Publié le le <?= $data['creation_date_fr'] ?></em> </h5>
-                                
-                                <p>
-                                    <?= nl2br(htmlspecialchars($excerpt)) , ' [...]' ?>
-                                    <br />
-                                </p>
-                                <h6><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Lire la suite</a></h6>
-                            </div>
-                        </div>
-                <?php
-                }
-                $posts->closeCursor();
-                ?>
-            </aside>
-            <!-- /Sidebar -->
 
         </div>
     </div>  <!-- /container -->
