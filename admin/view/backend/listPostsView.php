bd782b7a89763627dba2d342dcd07bc6502c37f7
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-06 09:36:03 +0700

    modification indentation et fil d'arianne

--- admin/view/backend/listPostsView.php
+++ admin/view/backend/listPostsView.php
@@ -6,33 +6,31 @@
     <div class="container">
 
         <ol class="breadcrumb">
-            <li><a href="index.php">Accueil</a></li>
-            <li class="active">Blog</li>
+            <li><a href="index.php">Administration</a></li>
+            <li class="active">Modifier/Supprimer</li>
         </ol>
 
         <div class="row">
-
             <?php
             while ($lastPost = $posts->fetch())
             {
             $excerpt = substr($lastPost['content'], 0, 1500);
             ?>
             <div class="col-md-12 maincontent">
-                <header class="page-header">
+                <div class="page-header">
                     <h1 class="page-title"><?= htmlspecialchars($lastPost['title']) ?></h1>
-                                 <h5><em>Publié le le <?= $lastPost['creation_date_fr'] ?></em> </h5>
-                </header>
-                                <p>
-                                    <?= nl2br(htmlspecialchars($excerpt)), ' [...]' ?>
-                                    <br />
-                                </p>
-                                <a class="btn btn-warning" href="index.php?action=updatePost&amp;id=<?= $post['id'] ?>">Modifier</a>
-                                <a class="btn btn-danger" href="index.php?action=deletePost&amp;id=<?= $post['id'] ?>">Supprimer</a>
+                    <h5><em>Publié le le <?= $lastPost['creation_date_fr'] ?></em> </h5>
+                </div>
+                <p>
+                    <?= nl2br(htmlspecialchars($excerpt)), ' [...]' ?>
+                    <br>
+                </p>
+                <a class="btn btn-warning" href="index.php?action=updatePost&amp;id=<?= $post['id'] ?>">Modifier</a>
+                <a class="btn btn-danger" href="index.php?action=deletePost&amp;id=<?= $post['id'] ?>">Supprimer</a>
             </div>
             <?php
             }
             ?>
-
         </div>
     </div>  <!-- /container -->
 
