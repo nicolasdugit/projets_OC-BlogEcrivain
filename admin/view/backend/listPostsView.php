37a4645a71f30c4edb767ca42f398f64cbb9594b
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-08 14:38:30 +0700

    rajout htmlspecialchars

--- admin/view/backend/listPostsView.php
+++ admin/view/backend/listPostsView.php
@@ -18,7 +18,7 @@
             ?>
             <div class="col-md-12 maincontent">
                 <div class="page-header">
-                    <h1 class="page-title"><?= ($post['title']) ?></h1>
+                    <h1 class="page-title"><?= htmlspecialchars($post['title']) ?></h1>
                     <h5><em>Publié le le <?= $post['creation_date_fr'] ?></em> </h5>
                 </div>
                 <p>
