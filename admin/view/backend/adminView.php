fef05bab07f3a9e22bdece6eb0c1a945b740d033
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-08 09:54:54 +0700

    quelques tests visuels

--- admin/view/backend/adminView.php
+++ admin/view/backend/adminView.php
@@ -7,20 +7,20 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-4">
-            	<h3>Actions</h3>
+            	<p class="lead text-uppercase">Actions</p>
 				<h4><a href="index.php?action=createPost">Nouvel article</a></h4>
 				<h4><a href="index.php?action=listPost">Modifier/Supprimer un article</a></h4>
 				<h4><a href="../index.php">Retour a l'accueil</a></h4>
             </div>
             <div class="col-sm-4">
-            	<h3>Commentaires signalés</h3>
+            	<p class="lead">Commentaires signalés</p>
                 <?php 
                 while ($data = $reportedComments->fetch()) {
                     if ($data['comment_report'] == 1) 
                     {
                         ?>
-                        <h3><?= $data['author']  ?></h3>
-                        <p><?= nl2br($data['comment']) ?></p>
+                        <h4 class="bg-warning">Par : <?= $data['author']  ?></h4>
+                        <p class="bg-danger"><?= nl2br($data['comment']) ?></p>
                         <a href="index.php?action=validateComment&amp;com_id=<?= $data['id'] ?>" class="btn btn-success">Valider</a>
                         <a href="index.php?action=deleteComment&amp;com_id=<?= $data['id'] ?>" class="btn btn-warning">Supprimer</a>
                         <?php
@@ -30,14 +30,14 @@
                 ?>
             </div>
             <div class="col-sm-4">
-                <h3>Commentaires à modérer</h3>
+                <p class="lead text-uppercase">Commentaires à modérer</p>
                 <?php 
                 while ($data = $toValidateComments->fetch()) {
                     if ($data['comment_verify'] == 0 && $data['comment_report'] == 0) 
                     {
                         ?>
-                        <h3><?= $data['author']  ?></h3>
-                        <p><?= nl2br($data['comment']) ?></p>
+                        <h4 class="bg-warning">Par : <?= $data['author']  ?></h4>
+                        <p class="bg-info"><?= nl2br($data['comment']) ?></p>
                         <a href="index.php?action=validateComment&amp;com_id=<?= $data['id'] ?>" class="btn btn-success">Valider</a>
                         <a href="index.php?action=deleteComment&amp;com_id=<?= $data['id'] ?>" class="btn btn-warning">Supprimer</a>
                         <?php
